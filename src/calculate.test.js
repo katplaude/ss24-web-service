f12af5f8d3e0dc29d34d3daa0d20eda8a605148b
katplaude <3120618@STUD.SRH-CAMPUS-BERLIN.DE>
2024-04-11 17:48:36 +0200

    first

--- src/calculate.test.js
+++ src/calculate.test.js
@@ -1,6 +1,23 @@
 import {factorial} from './calculate';
 import {test, describe, expect} from "@jest/globals"; // this is optional, all three are global variables im runner scope
 
-test ('5! is 120', () => {
-    expect(factorial(5)).toBe(120)
-});
+describe('factorial', () => {
+    test('5! is 120', () => {
+        expect(factorial(5)).toBe(120)
+    });
+
+    test('0! is 1', () => {
+        expect(factorial(0)).toBe(1)
+
+    });
+
+    test('Factorial og negative int is throwing exception', () => {
+        expect(() => {
+            factorial(-5);
+        }).toThrow();
+    });
+})
+
+describe('product', () => {
+
+})
